7d872b140fd5819b54ce79647e668f2839c5a227
Rupert Broad <rupert.broad@gmail.com>
2019-02-04 09:07:02 +0100

    change service to fargate add tags

--- fargate/service.tf
+++ fargate/service.tf
@@ -3,6 +3,7 @@ resource "aws_ecs_service" "flaskapi" {
   cluster         = "${aws_ecs_cluster.main.id}"
   task_definition = "${aws_ecs_task_definition.flaskapi.arn}"
   desired_count   = "${var.desired_count}"
+  launch_type     = "FARGATE"
 
   load_balancer {
     target_group_arn = "${var.alb_single_tg_arns[0]}"
@@ -18,4 +19,13 @@ resource "aws_ecs_service" "flaskapi" {
     subnets         = ["${var.public_subnets}"]
     security_groups = ["${var.flaskapi_sg_id}"]
   }
+
+  # Tags
+  tags = "${merge(
+    var.default_tags, 
+    map(
+      "Name", "${local.service_name}",
+      "Workspace", "${terraform.workspace}"
+    )
+  )}"
 }
